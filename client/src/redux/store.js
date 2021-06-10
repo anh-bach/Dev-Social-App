49bbd2b41b9239ebcc6998c7f5efa47b69b5e4df
AlvinBach <quocanh1990bach@gmail.com>
2021-06-10 22:24:56 +0300

    deploy

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
@@ -9,7 +9,7 @@ const middlewares = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
-  composeWithDevTools(applyMiddleware(...middlewares))
+  applyMiddleware(...middlewares)
 );
 
 export default store;
