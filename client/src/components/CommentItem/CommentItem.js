410312a1a5d605dbc1e4564aa326ced7b411c3c9
AlvinBach <quocanh1990bach@gmail.com>
2021-06-11 08:33:17 +0300

    fixing comment image

--- client/src/components/CommentItem/CommentItem.js
+++ client/src/components/CommentItem/CommentItem.js
@@ -2,16 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Image from 'react-bootstrap/Image';
 
 const CommentItem = ({ text, name, avatar, user }) => {
   return (
     <Row className='mt-2 posted-comment-area'>
-      <Col xs={1} className="posted-comment-avatar">
+      <Col xs={1} className='posted-comment-avatar'>
         <Link to={`/profile/${user}`}>
           <div
             style={{
-              backgroundImage: `url(${process.env.PUBLIC_URL}/assets/images/posts/${avatar})`,
+              backgroundImage: `url(https://iamanhrecipeapp.s3.amazonaws.com/${avatar})`,
               backgroundPosition: 'center',
               backgroundSize: 'cover',
               backgroundRepeat: 'no-repeat',
@@ -23,7 +22,7 @@ const CommentItem = ({ text, name, avatar, user }) => {
         </Link>
       </Col>
       <Col xs={11} className='posted-comment'>
-        <div className="posted-comment-content">
+        <div className='posted-comment-content'>
           <Link to={`/profile/${user}`}>
             <small>{name}</small>
           </Link>
